03c0832aa7f9be808fec005f00379945464e99b4
Mattsun20030101 <arashi5lovers@gmail.com>
2024-06-16 20:02:08 +1000

    ロゴを消しました!

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <>     
       <h1>Gitの練習用のアプリです!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -29,7 +21,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
